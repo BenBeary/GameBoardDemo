8d56876105d161e8db5f379aea8472871eaecc0b
BenBeary <120347482+BenBeary@users.noreply.github.com>
2024-09-13 02:24:53 -0700

    Bug Fixxes

--- F2024 Platformer Demo/Assets/Script/DoubleJumpRestore.cs
+++ F2024 Platformer Demo/Assets/Script/DoubleJumpRestore.cs
@@ -8,20 +8,35 @@ public class DoubleJumpRestore : MonoBehaviour
     [SerializeField] float bobFrequency;
     [SerializeField] float rotationSpeed = 0.5f;
 
+
+    private void Start()
+    {
+        PlayerController.playerReset += resetAbility;
+    }
+
     private void FixedUpdate()
     {
         transform.Translate(Vector3.up * Mathf.Cos(Time.time * bobFrequency) * bobIntensity);
         transform.GetChild(0).transform.Rotate(0, 0, rotationSpeed);
     }
 
+    
+    private void resetAbility()
+    {
+        GetComponent<BoxCollider2D>().enabled = true;
+        GetComponent<ParticleSystem>().Play();
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject == PlayerController.instance.gameObject)
+        if (collision.gameObject == PlayerController.instance.gameObject)
         {
             PlayerController.instance.doubleJump = true;
             PlayerController.instance.dotCount++;
-            Destroy(gameObject);
+            GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<ParticleSystem>().Stop();
+            transform.GetChild(0).gameObject.SetActive(false);
         }
     }
 }
