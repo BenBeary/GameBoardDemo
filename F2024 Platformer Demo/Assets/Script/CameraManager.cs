ec4e279dee6d546b493364b4b64d4b772bb63ce0
BenBeary <120347482+BenBeary@users.noreply.github.com>
2025-02-24 12:58:07 -0800

    Camera System V1

--- F2024 Platformer Demo/Assets/Script/CameraManager.cs
+++ F2024 Platformer Demo/Assets/Script/CameraManager.cs
@@ -10,15 +10,24 @@ public class CameraManager : MonoBehaviour
     [SerializeField] int paddingX;
     [Range(0, 100)]
     [SerializeField] int paddingY;
+    [Min(0)]
+    [SerializeField] float speedDampening;
     [SerializeField] float catchUpSpeed;
+    [SerializeField] bool dynamicSpeed;
+    [SerializeField] float lookAheadDistance;
+    [SerializeField] float lookAheadSpeed;
     public static bool frozen;
-
+    [Range(0,16f)]
+    [SerializeField] float deadZone = 1f;
     [Header("Debug")]
-    //public bool dontMove;
+    [SerializeField] Vector3 clampedMove;
     public Transform target;
+    
+    Vector3 targetLastPos;
+
     Camera cam
     {
-        get { return GetComponent<Camera>(); }
+        get { return Camera.main; }
     }
     Vector2 percentConvert
     {
@@ -49,36 +58,45 @@ public class CameraManager : MonoBehaviour
     }
 
 
+    private void FixedUpdate()
+    {
+        targetLastPos = transform.position;
+    }
+
+
     void PutTargetInBox()
     {
-        if(!SceneController.Instance || !SceneController.Instance.activeChunk) 
+        if (!SceneController.Instance || !SceneController.Instance.activeChunk)
         {
             Debug.LogWarning("No Scene Controller to apply Clamp (Camera will no Move)");
-            return; 
+            return;
         }
 
 
         Vector2 moveInput = Vector2.zero;
+        Vector4 boundary = new Vector4(transform.position.x - percentConvert.x, transform.position.y - percentConvert.y, transform.position.x + percentConvert.x, transform.position.y + percentConvert.y);
+
 
         // Outside of border box
 
-        if(target.position.x < transform.position.x - percentConvert.x || target.position.x > transform.position.x + percentConvert.x)
+        if (target.position.x < boundary.x || target.position.x > boundary.z)
         {
-            moveInput.x = -Vector2.Distance(target.position, transform.position) * catchUpSpeed / 10f;
-            if (target.position.x > transform.position.x + percentConvert.x) moveInput.x *= -1f;
-           
+            moveInput.x = target.position.x > boundary.z ? 1f : -1f;
+
         }
-        if (target.position.y < transform.position.y - percentConvert.y || target.position.y > transform.position.y + percentConvert.y)
+        if (target.position.y < boundary.y || target.position.y > boundary.w)
         {
-            moveInput.y = -Vector2.Distance(target.position, transform.position) * catchUpSpeed / 10f;
-            if (target.position.y > transform.position.y + percentConvert.y) moveInput.y *= -1f;
-            
+            moveInput.y = target.position.y > boundary.w ? 1f : -1f;
+
         }
+
+
+
 
 
         // movement with current chunks restrictions
 
-        Vector3 clampedMove = transform.position + (Vector3)moveInput;
+        clampedMove = transform.position + (Vector3)moveInput;
 
         clampedMove.x = Mathf.Clamp(clampedMove.x,
                                         SceneController.Instance.activeChunk.transform.position.x - SceneController.Instance.activeChunk.cameraClampArea.x / 2f,
@@ -86,7 +104,23 @@ public class CameraManager : MonoBehaviour
         clampedMove.y = Mathf.Clamp(clampedMove.y,
                                         SceneController.Instance.activeChunk.transform.position.y - SceneController.Instance.activeChunk.cameraClampArea.y / 2f,
                                         SceneController.Instance.activeChunk.transform.position.y + SceneController.Instance.activeChunk.cameraClampArea.y / 2f);
-        transform.Translate(clampedMove - transform.position);
+
+
+        if (dynamicSpeed)
+        {
+            catchUpSpeed = Vector2.Distance(target.position, transform.position) * 2;
+
+            if (target.position.x < boundary.x - cam.pixelWidth / 128 + deadZone || target.position.x > boundary.z + cam.pixelWidth / 128 - deadZone ||
+                target.position.y < boundary.y - cam.pixelHeight / 128 + deadZone || target.position.y > boundary.w + cam.pixelHeight / 128 - deadZone) // Player is out of camera
+            {
+                catchUpSpeed *= 10f;
+                Debug.Log("Player Out of Camera View");
+            }
+
+        }
+
+        transform.position = Vector3.Lerp(transform.position, clampedMove, (catchUpSpeed / (1+speedDampening)) * Time.deltaTime);
+        
     }
 
 
@@ -126,6 +160,9 @@ public class CameraManager : MonoBehaviour
         Gizmos.color = Color.blue;
         Gizmos.DrawLine((Vector2)verticalPoint, new Vector2(verticalPoint.z, verticalPoint.y));
         #endregion
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, cam.pixelRect.size / 32 / 2 - Vector2.one * deadZone);
 
     }
 
